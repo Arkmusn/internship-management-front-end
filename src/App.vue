b000db52be6b1fc2d993906e5f1b73cf264df74d
Arkmusn <ark.c.mf@gmail.com>
2017-12-22 19:57:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  .flex-container {
+    display: flex;
+  }
+  .form-box {
+    border: 1px solid #DCDFE6;
+    border-radius: 10px;
+  }
+</style>
---
+++ src/module/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="login">
+    <el-row type="flex" justify="center">
+      <el-col :span="6">
+        <div class="form-box">
+          <el-form label-width="60px" :model="form" ref="form">
+            <el-form-item label="用户名">
+              <el-input v-model="form.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input type="password" v-model="form.password"></el-input>
+            </el-form-item>
+            <div class="flex-container login-box">
+              <el-button type="primary" class="line-button" @click="handleClickLogin">登录</el-button>
+            </div>
+          </el-form>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        form: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    method: {
+      handleClickLogin() {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .line-button {
+    width: 90%;
+    margin: 0 auto;
+  }
+  .login-box {
+    justify-content: center;
+  }
+</style>
